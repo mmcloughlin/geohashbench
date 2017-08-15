84574ce0591f754483ca6511236482471e7fd413
Michael McLoughlin <mmcloughlin@gmail.com>
2017-08-14 19:12:06 -0700

    github.com/gansidui/geohash

--- benchmarks_test.go
+++ benchmarks_test.go
@@ -2,33 +2,40 @@ package geohashbench
 
 import (
 	"testing"
-	mmcloughlin "github.com/mmcloughlin/geohash"
+
 	tomihiltunen "github.com/TomiHiltunen/geohash-golang"
+	gansidui "github.com/gansidui/geohash"
+	mmcloughlin "github.com/mmcloughlin/geohash"
 )
 
 func BenchmarkMmcloughlinEncode(b *testing.B) {
-    points := RandomPoints(b.N)
-    b.ResetTimer()
-    for i := 0; i < b.N; i++ {
-        mmcloughlin.Encode(points[i][0], points[i][1])
-    }
+	points := RandomPoints(b.N)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		mmcloughlin.Encode(points[i][0], points[i][1])
+	}
 }
-
 
 func BenchmarkMmcloughlinEncodeInt(b *testing.B) {
-    points := RandomPoints(b.N)
-    b.ResetTimer()
-    for i := 0; i < b.N; i++ {
-        mmcloughlin.EncodeInt(points[i][0], points[i][1])
-    }
+	points := RandomPoints(b.N)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		mmcloughlin.EncodeInt(points[i][0], points[i][1])
+	}
 }
-
 
 func BenchmarkTomihiltunenEncode(b *testing.B) {
-    points := RandomPoints(b.N)
-    b.ResetTimer()
-    for i := 0; i < b.N; i++ {
-        tomihiltunen.Encode(points[i][0], points[i][1])
-    }
+	points := RandomPoints(b.N)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		tomihiltunen.Encode(points[i][0], points[i][1])
+	}
 }
 
+func BenchmarkGansiduiEncode(b *testing.B) {
+	points := RandomPoints(b.N)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		gansidui.Encode(points[i][0], points[i][1], 12)
+	}
+}
