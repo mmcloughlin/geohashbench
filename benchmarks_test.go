222eac34aa2ca5f719f56857cb1c9565a724f35c
Michael McLoughlin <mmcloughlin@gmail.com>
2017-08-14 19:27:58 -0700

    add a load more pacakges

--- benchmarks_test.go
+++ benchmarks_test.go
@@ -3,9 +3,16 @@ package geohashbench
 import (
 	"testing"
 
+	codefor "github.com/Codefor/geohash"
 	tomihiltunen "github.com/TomiHiltunen/geohash-golang"
+	broady "github.com/broady/gogeohash"
+	bsm "github.com/bsm/geohashi"
+	corsc "github.com/corsc/go-geohash"
+	ezzkoram "github.com/ezzkoram/geohash"
+	fanixk "github.com/fanixk/geohash"
 	gansidui "github.com/gansidui/geohash"
 	mmcloughlin "github.com/mmcloughlin/geohash"
+	pierrre "github.com/pierrre/geohash"
 )
 
 func BenchmarkMmcloughlinEncode(b *testing.B) {
@@ -37,5 +44,61 @@ func BenchmarkGansiduiEncode(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		gansidui.Encode(points[i][0], points[i][1], 12)
+	}
+}
+
+func BenchmarkPierrreEncode(b *testing.B) {
+	points := RandomPoints(b.N)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		pierrre.Encode(points[i][0], points[i][1], 12)
+	}
+}
+
+func BenchmarkBroadyEncode(b *testing.B) {
+	points := RandomPoints(b.N)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		broady.Encode(points[i][0], points[i][1])
+	}
+}
+
+func BenchmarkBsmEncodeInt(b *testing.B) {
+	points := RandomPoints(b.N)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		bsm.Encode(points[i][0], points[i][1])
+	}
+}
+
+func BenchmarkCorscEncodeInt52(b *testing.B) {
+	points := RandomPoints(b.N)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		corsc.EncodeInt(points[i][0], points[i][1], 52)
+	}
+}
+
+func BenchmarkEzzkoramEncodeInt(b *testing.B) {
+	points := RandomPoints(b.N)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ezzkoram.FromCoordinates(points[i][0], points[i][1]).Hash()
+	}
+}
+
+func BenchmarkCodeforEncode(b *testing.B) {
+	points := RandomPoints(b.N)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		codefor.Encode(points[i][0], points[i][1])
+	}
+}
+
+func BenchmarkFanixkEncode(b *testing.B) {
+	points := RandomPoints(b.N)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		fanixk.Encode(points[i][0], points[i][1])
 	}
 }
